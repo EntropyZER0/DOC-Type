1d583c4783634a222d21a8b7d0722811f55755da
EntropyZER0 <saullocesarc@gmail.com>
2026-05-06 23:49:00 -0600

    Refactor script.js to add dynamic effects and messages

--- script.js
+++ script.js
@@ -8,8 +8,140 @@
  // Función que cambia el color de fondo del menú cuando se desplaza
  window.addEventListener("scroll", () => {
      if (window.scrollY > headerHeight) {
-         menu.style.backgroundColor = "rgb(	128, 128, 128)"; // Cambia el color de fondo a azul
+         menu.style.backgroundColor = "rgba(0, 0, 0, 0)"; // Cambia el color de fondo a azul
      } else {
          menu.style.backgroundColor = "rgba(0, 0, 0, 0)"; // Vuelve al fondo transparente
      }
  });
+
+// Activar el efecto de manera aleatorea 
+const glitch = document.querySelector(".glitch-screen");
+
+setInterval(() => {
+
+    const random = Math.random();
+
+    if(random > 0.92){
+
+        glitch.style.opacity = "1";
+
+        glitch.style.background =
+        `url('img/noise.gif')`;
+
+        setTimeout(() => {
+            glitch.style.opacity = "0";
+        }, 120);
+
+    }
+
+}, 3000);
+/*Cambiar fondos*/ 
+const fondos = [
+    "img/bg1.gif",
+    "img/bg2.gif",
+    "img/bg3.gif"
+];
+
+setInterval(() => {
+
+    const random =
+    fondos[Math.floor(Math.random() * fondos.length)];
+
+    document.body.style.backgroundImage =
+    `url(${random})`;
+
+}, 30000);
+
+// TESt 
+
+const sombra =
+document.createElement("div");
+
+sombra.style.position = "fixed";
+sombra.style.width = "20px";
+sombra.style.height = "20px";
+sombra.style.borderRadius = "50%";
+sombra.style.background = "rgba(255,255,255,0.1)";
+sombra.style.pointerEvents = "none";
+
+document.body.appendChild(sombra);
+
+document.addEventListener("mousemove", e => {
+
+    sombra.style.left = e.clientX + "px";
+    sombra.style.top = e.clientY + "px";
+
+});
+
+//TEST 2
+setInterval(() => {
+
+    document.body.style.transform =
+    `translateX(${Math.random()*4-2}px)`;
+
+    setTimeout(() => {
+
+        document.body.style.transform =
+        "translateX(0px)";
+
+    }, 40);
+
+}, 3000);
+
+// TEST 3
+const mensajes = [
+
+    "THE HALLWAY CONTINUES",
+    "SIGNAL DETECTED",
+    "DO NOT TRUST THE STAIRS",
+    "SUBJECT STILL ACTIVE",
+    "SOMEONE IS INSIDE",
+    "LEVEL BELOW UNAVAILABLE",
+    "THE DOOR MOVED",
+    "NO RESPONSE FROM FLOOR 3",
+    "...",
+    "Lobos de SOFTWARE"
+
+];
+
+const transmission =
+document.getElementById("transmission");
+
+function escribirMensaje(texto){
+
+    transmission.innerHTML = "";
+
+    let i = 0;
+
+    const intervalo = setInterval(() => {
+
+        transmission.innerHTML += texto.charAt(i);
+
+        i++;
+
+        if(i >= texto.length){
+
+            clearInterval(intervalo);
+
+            setTimeout(() => {
+
+                transmission.innerHTML = "";
+
+            }, 4000);
+
+        }
+
+    }, 70);
+
+}
+
+setInterval(() => {
+
+    const random =
+    mensajes[
+        Math.floor(Math.random() * mensajes.length)
+    ];
+
+    escribirMensaje(random);
+
+}, 10000);
